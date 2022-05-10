cf556c95352ea1e00ff92f94ac5b7521164dae56
henrque <henrque02@gmail.com>
2022-05-10 16:11:56 -0300

    PI-34: correção no modal para confirmação

--- src/app/components/espera/espera.component.ts
+++ src/app/components/espera/espera.component.ts
@@ -3,29 +3,38 @@ import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 import { WebSocketService } from 'src/app/services/web-socket.service';
 import Cookies from 'js-cookie';
-
+import { bootstrap } from 'bootstrap';
+import { Modal } from 'bootstrap';
 @Component({
   selector: 'app-espera',
   templateUrl: './espera.component.html',
   styleUrls: ['./espera.component.scss'],
 })
 export class EsperaComponent implements OnInit {
+  eventObs: any;
   constructor(
     private socketService: WebSocketService,
     private router: Router,
     private dataService: DataService
     ) {
-    this.socketService.onEvent('iniciar').subscribe((data) => {
-
+      this.eventObs = this.socketService.onEvent('iniciar').subscribe((data) => {
+        var myModal = new Modal(document.getElementById('exampleModal'));
+        myModal.show();
     });
   }
 
   ngOnInit(): void {}
   conectar() {
+    this.eventObs.unsubscribe();
+    var myModal = new Modal(document.getElementById('exampleModal'));
+    myModal.hide();
     this.router.navigate(['/atendimento']);
   }
   desconectar() {
-    this.dataService.sairFila(Cookies.get("ra")).subscribe();
+    this.eventObs.unsubscribe();
+    var myModal = new Modal(document.getElementById('exampleModal'));
+    myModal.hide();
+    this.dataService.sairFila(Cookies.get('ra')).subscribe();
     this.router.navigate(['/']);
   }
 }
