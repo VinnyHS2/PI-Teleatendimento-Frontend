79228db5815f9492d20380264980014944e1ec16
henrque <henrque02@gmail.com>
2022-06-04 17:38:41 -0300

    PI-28: tratando encerramento do atendimento pelo professor

--- src/app/components/teleatendimento-aluno/teleatendimento-aluno.component.ts
+++ src/app/components/teleatendimento-aluno/teleatendimento-aluno.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
+import { NotificationService } from 'src/app/services/notificacation.service';
 import { VideoService } from 'src/app/services/video.service';
+import { WebSocketService } from 'src/app/services/web-socket.service';
 
 @Component({
   selector: 'app-teleatendimento-aluno',
@@ -9,20 +11,36 @@ import { VideoService } from 'src/app/services/video.service';
   styleUrls: ['./teleatendimento-aluno.component.scss']
 })
 export class TeleatendimentoAlunoComponent implements OnInit {
+  eventObs: any;
+  aluno: string = " "
 
   constructor(
     private videoService: VideoService,
     private dataService: DataService,
-    private router: Router
+    private notificationService: NotificationService,
+    private router: Router,
+    private socketService: WebSocketService
+
   ) {
+    this.aluno = this.dataService.getRA();
+    this.eventObs = this.socketService.onEvent('finalizar-professor').subscribe((data) => {
+      this.videoService.hangupAluno();
+      this.socketService.disconnect().subscribe();
+      this.notificationService.showInfo(
+        'O professor finalizou o atendimento.'
+      );
+      this.router.navigate(['/atendimento/avaliacao']);
+    });
   }
-  
-  aluno: string = " "
-  
+
+
   sair(){
-    this.router.navigate(['/']);
+    // TODO: chamar endpoint para avisar o professor que o aluno saiu
+    this.videoService.hangupAluno();
+    this.socketService.disconnect().subscribe();
+    this.router.navigate(['/atendimento/avaliacao']);
   }
-  
+
   ngOnInit(): void {
     this.videoService.joinSessionAluno(this.dataService.getRA(), this.dataService.getIdSala()).subscribe();
   }
